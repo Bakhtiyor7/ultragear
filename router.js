51be4cf0e602b8ff714d4fff4af61631c5670034
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-09-23 01:14:50 +0900

    feat: member controllerlarni qurish

--- router.js
+++ router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
 
-router.get("/", function(req, res) {
-    res.send("Home sahifasidasiz");
-});
+// memberga dahldor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout)
 
 router.get("/menu", (req, res) => {
     res.send("Menu safifasidasiz");
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config(); // ESDAN CHIQARMANG !!!!!!!!!
 
 const http = require("http");
 
