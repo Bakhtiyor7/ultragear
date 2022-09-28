ad9188e31fdd4760c832d72684f7c2e147e7330e
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-09-28 19:57:24 +0900

    feat: bcrypt: package asosida passwordni hash qilish

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -3,36 +3,34 @@ const Member = require("../models/Member");
 let memberController = module.exports;
 
 memberController.signup = async (req, res) => {
-    try {
-        console.log("POST: cont/signup");
-        const data = req.body,
-        member = new Member(),
-        new_member = await member.signupData(data);
+  try {
+    console.log("POST: cont/signup");
+    const data = req.body,
+      member = new Member(),
+      new_member = await member.signupData(data);
 
-        res.json({state: 'succeed', data: new_member });
-    } catch(err) {
-        console.log(`ERROR, cont/signup, ${err.message}`);
-        res.json({state: 'fail', message: err.message });
-    }
+    res.json({ state: "succeed", data: new_member });
+  } catch (err) {
+    console.log(`ERROR, cont/signup, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
 memberController.login = async (req, res) => {
-    try {
-        console.log("POST: cont/login");
-        const data = req.body,
-        member = new Member(),
-        result = await member.loginData(data);
+  try {
+    console.log("POST: cont/login");
+    const data = req.body,
+      member = new Member(),
+      result = await member.loginData(data);
 
-        res.json({state: 'succeed', data: result });
-    } catch(err) {
-        console.log(`ERROR, cont/login, ${err.message}`);
-        res.json({state: 'fail', message: err.message });
-    }
+    res.json({ state: "succeed", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/login, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
 memberController.logout = (req, res) => {
-    console.log("GET cont.logout");
-    res.send("logout sahifasidasiz");
+  console.log("GET cont.logout");
+  res.send("logout sahifasidasiz");
 };
-
-
--- models/Member.js
+++ models/Member.js
@@ -1,58 +1,59 @@
 const MemberModel = require("../schema/member.model.js");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
+var bcrypt = require("bcryptjs");
 
 class Member {
-    constructor() {
-        this.memberModel = MemberModel;
-    }
+  constructor() {
+    this.memberModel = MemberModel;
+  }
 
-async signupData(input) {
+  async signupData(input) {
     try {
-        const new_member = new this.memberModel(input); 
+      const salt = await bcrypt.genSalt();
+      input.mb_password = await bcrypt.hash(input.mb_password, salt);
+      const new_member = new this.memberModel(input);
 
-        let result
-        try {
+      let result;
+      try {
         result = await new_member.save();
-        } catch(mongo_err) {
-            console.log(mongo_err);
-            throw new Error(Definer.auth_err1);
-        }
-        
-        console.log(result);
+      } catch (mongo_err) {
+        console.log(mongo_err);
+        throw new Error(Definer.auth_err1);
+      }
 
-        result.mb_password = "";
-        return result;
-    } catch(err) {
-        throw err;
+      console.log(result);
+
+      result.mb_password = "";
+      return result;
+    } catch (err) {
+      throw err;
     }
-}
+  }
 
-async loginData(input) {
+  async loginData(input) {
     try {
-        const member = await this.memberModel
-        .findOne(
-            {mb_nick: input.mb_nick}, 
-            {mb_nick: 1, mb_password: 1})
+      const member = await this.memberModel
+        .findOne({ mb_nick: input.mb_nick }, { mb_nick: 1, mb_password: 1 })
         .exec();
 
-        assert.ok(member, Definer.err_auth3);
-        
-        const isMatch = input.mb_password === member.mb_password;
-        assert.ok(isMatch, Definer.err_auth4);
+      assert.ok(member, Definer.err_auth3);
 
-        return await this.memberModel
+      const isMatch = await bcrypt.compare(
+        input.mb_password,
+        member.mb_password
+      );
+      assert.ok(isMatch, Definer.err_auth4);
+
+      return await this.memberModel
         .findOne({
-            mb_nick: input.mb_nick
+          mb_nick: input.mb_nick,
         })
         .exec();
-    } catch(err) {
-        throw err;
+    } catch (err) {
+      throw err;
     }
+  }
 }
-
-
-};
-
 
 module.exports = Member;
