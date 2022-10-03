be60cffb28d10ee0997f91eb88d7a9d4b41f1cf5
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-10-03 17:49:28 +0900

    feat: multer single image upload process

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,8 +12,9 @@ productController.getAllProducts = async (req, res) => {
 productController.addNewProduct = async (req, res) => {
   try {
     console.log("POST: cont/addNewProduct");
-    console.log(req.member);
     // TODO: product creation develop
+
+    res.send("OK");
   } catch {
     console.log(`ERROR, cont/addNewProduct, ${err.message}`);
   }
