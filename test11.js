306e729bf83485cee2feb61b8473635a877b1764
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-10-18 23:38:19 +0900

    fix: ERROR on restaurant-menu.ejs

---
+++ test11.js
@@ -0,0 +1,9 @@
+const arr1 = ["ab", "cd"];
+const arr2 = ["hello", "hi"];
+const arr3 = ["me", "one"];
+
+function convertorFunction(a) {
+  return a.concat(arr1, arr2, arr3);
+}
+
+console.log(convertorFunction([]));
