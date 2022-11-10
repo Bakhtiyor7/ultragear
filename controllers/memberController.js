9db32986764bbcf143d74b780899502eba83ee25
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-11-10 15:11:51 +0900

    feat: create jwt tokens and enable cookie-parser

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,6 +1,9 @@
 const Member = require("../models/Member");
 
 let memberController = module.exports;
+const jwt = require("jsonwebtoken");
+const assert = require("assert");
+const Definer = require("../lib/mistake");
 
 memberController.signup = async (req, res) => {
   try {
@@ -9,8 +12,12 @@ memberController.signup = async (req, res) => {
       member = new Member(),
       new_member = await member.signupData(data);
 
-    //TODO: AUTHENTICATE BASED ON JWT
+    const token = memberController.createToken(new_member);
 
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
     console.log(`ERROR, cont/signup, ${err.message}`);
@@ -25,6 +32,13 @@ memberController.login = async (req, res) => {
       member = new Member(),
       result = await member.loginData(data);
 
+    const token = memberController.createToken(result);
+
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
+
     res.json({ state: "succeed", data: result });
   } catch (err) {
     console.log(`ERROR, cont/login, ${err.message}`);
@@ -35,4 +49,25 @@ memberController.login = async (req, res) => {
 memberController.logout = (req, res) => {
   console.log("GET cont.logout");
   res.send("logout sahifasidasiz");
+};
+
+// WEB token method
+
+memberController.createToken = (result) => {
+  try {
+    const upload_data = {
+      _id: result._id,
+      mb_nick: result.mb_nick,
+      mb_type: result.mb_type,
+    };
+
+    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+      expiresIn: "6h",
+    });
+
+    assert.ok(token, Definer.auth_err2);
+    return token;
+  } catch (err) {
+    throw err;
+  }
 };
