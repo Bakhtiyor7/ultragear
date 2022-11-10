4b2e6aa2ee7bb6550dfc3dc30b7bef12e692fce5
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-11-10 17:35:41 +0900

    feat: getChosenMember rest API development part one

--- models/Member.js
+++ models/Member.js
@@ -2,6 +2,7 @@ const MemberModel = require("../schema/member.model.js");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
 var bcrypt = require("bcryptjs");
+const { shapeIntoMongooseObjectId } = require("../lib/config.js");
 
 class Member {
   constructor() {
@@ -56,6 +57,29 @@ class Member {
           mb_nick: input.mb_nick,
         })
         .exec();
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getChosenMemberData(member, id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+      console.log("member:::", member);
+
+      if (member) {
+        // condition if not seen before
+      }
+
+      const result = await this.memberModel
+        .aggregate([
+          { $match: { _id: id, mb_status: "ACTIVE" } },
+          { $unset: "mb_password" },
+        ])
+        .exec();
+
+      assert.ok(result, Definer.general_err2);
+      return result[0];
     } catch (err) {
       throw err;
     }
