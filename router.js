6fb44343cb52ce02197b68551b3752a8e02c0d80
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-11-24 19:23:23 +0900

    feat: develop getMemberFollowings rest api logic

--- router.js
+++ router.js
@@ -109,5 +109,6 @@ router.post(
   memberController.retrieveAuthMember,
   followController.unsubscribe
 );
+router.get("/follow/followings", followController.getMemberFollowings);
 
 module.exports = router;
