42ff220ca89fa4dd39cfdd60f95c88ef4d62a278
Baxtiyor <baxtiyor.axatov77@gmail.com>
2022-11-26 19:22:20 +0900

    feat: develop likeMemberChosen api business logic

--- models/Member.js
+++ models/Member.js
@@ -7,6 +7,7 @@ const {
   lookup_auth_member_following,
 } = require("../lib/config.js");
 const View = require("./View.js");
+const Like = require("./Like.js");
 
 class Member {
   constructor() {
@@ -115,6 +116,61 @@ class Member {
       }
 
       return true;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async viewChosenItemByMember(member, view_ref_id, group_type) {
+    try {
+      view_ref_id = shapeIntoMongooseObjectId(view_ref_id);
+      const mb_id = shapeIntoMongooseObjectId(member._id);
+
+      const view = new View(mb_id);
+      const isValid = await view.validateChosenTarget(view_ref_id, group_type);
+      console.log("isValid:", isValid);
+      assert.ok(isValid, Definer.general_err2);
+
+      // logged user has seen target before
+      const doesExist = await view.checkViewExistence(view_ref_id);
+      console.log("doesExist:::", doesExist);
+
+      if (!doesExist) {
+        const result = await view.insertMemberView(view_ref_id, group_type);
+        assert.ok(result, Definer.general_err1);
+      }
+
+      return true;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async likeChosenItemByMember(member, like_ref_id, group_type) {
+    try {
+      like_ref_id = shapeIntoMongooseObjectId(like_ref_id);
+      const mb_id = shapeIntoMongooseObjectId(member._id);
+
+      const like = new Like(mb_id);
+      const isValid = await like.validateTargetItem(like_ref_id, group_type);
+      assert.ok(isValid, Definer.general_err2);
+
+      //doesExist
+
+      const doesExist = await like.checkLikeExistence(like_ref_id);
+      console.log("doesExist:::", doesExist);
+
+      let data = doesExist
+        ? await like.removeMemberLike(like_ref_id, group_type)
+        : await like.insertMemberLike(like_ref_id, group_type);
+      assert.ok(data, Definer.general_err1);
+
+      const result = {
+        like_group: data.like_group,
+        like_ref_id: data.like_ref_id,
+        like_status: doesExist ? 0 : 1,
+      };
+      return result;
     } catch (err) {
       throw err;
     }
