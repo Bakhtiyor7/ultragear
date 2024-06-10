6e0a369624befeee54877ba76242d3133875437b
BAKHTIYOR AKHATOV <85681712+Bakhtiyor7@users.noreply.github.com>
2024-06-10 21:06:51 +0900

    feat: (app.js) save notes for prep

--- app.js
+++ app.js
@@ -13,44 +13,47 @@ const chatId = "548219471";
 const bot = new TelegramBot(botToken, { polling: false });
 
 let session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+const MongoDBStore = require("connect-mongodb-session")(session); // initializez mongodb store with express session
 const store = new MongoDBStore({
   uri: process.env.MONGO_URL,
-  collection: "sessions",
+  collection: "sessions", // saved into sessions module in MongoDB
 });
 
 //1.Opening codes
-app.use(express.static("public"));
-app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use(express.json());
+app.use(express.static("public")); // servers for HTML, CSS etc static files in public folder
+app.use("/uploads", express.static(__dirname + "/uploads")); // serves for files in uploads folder (http://domain.com/uploads)
+app.use(express.json()); // parses requests that have JSON body ==> makes req.body available
 app.use(
   express.urlencoded({
-    extended: true,
+    // makes post reqs (URL-encoded string ) available to read in the server
+    extended: true, // allows rich objects like forms and etc
   })
 );
 app.use(
+  // enables cross origin requests
   cors({
-    credentials: true,
-    origin: true,
+    credentials: true, // allows cookies
+    origin: true, // allows reqs from different origins
   })
 );
-app.use(cookieParser());
+app.use(cookieParser()); // parses cookies attached to the request. makes them available via req.cookies
 
 //2: Sessions
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET, // sign the session ID cookie
     cookie: {
       maxAge: 1000 * 60 * 30, //for 1 minutes
     },
-    store: store,
-    resave: true,
-    saveUninitialized: true,
+    store: store, // store the session in MongoDB store
+    resave: true, // keep the session alive
+    saveUninitialized: true, //Forces a session that is "uninitialized" (i.e., new but not modified) to be saved to the store.
   })
 );
 
-app.use(function(req, res, next) {
+// makes the member object from the session data available to all views/templates rendered during the request.
+app.use(function (req, res, next) {
   res.locals.member = req.session.member;
   next();
 });
@@ -73,7 +76,7 @@ const io = require("socket.io")(server, {
 });
 
 let online_users = 0;
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
   online_users++;
   console.log("New user, total:", online_users);
 
@@ -96,13 +99,13 @@ io.on("connection", function(socket) {
   socket.emit("greetMsg", { text: "welcome" });
   io.emit("infoMsg", { total: online_users });
 
-  socket.on("disconnect", function() {
+  socket.on("disconnect", function () {
     online_users--;
     socket.broadcast.emit("infoMsg", { total: online_users });
     console.log("client disconnected, total:", online_users);
   });
 
-  socket.on("createMsg", function(data) {
+  socket.on("createMsg", function (data) {
     console.log("createMsg:", data);
     io.emit("newMsg", data);
   });
